fb8e15000ce43edf9cff43e6953047438cd40c48
Eduardo Varela <eduardovarela139@gmail.com>
2025-11-11 15:37:07 -0600

    chore(lint-staged): updated lint staged configuration for ts and js files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 export default {
-    // For TS files, run typecheck without passing filenames so tsc uses tsconfig
     "*.{ts,mts}": ["pnpm format:check", "pnpm lint:check", () => "pnpm build:check"],
     "*.{js,mjs}": ["pnpm format:check", "pnpm lint:check"],
     "*.{json,yaml,yml}": ["pnpm format:check"],
